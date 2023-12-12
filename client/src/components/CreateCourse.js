066edcc3ff83f268257b7d2cfc5e6648ab86ec1b
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-12-12 13:39:22 -0800

    initial component set up

---
+++ client/src/components/CreateCourse.js
@@ -0,0 +1,48 @@
+<body>
+    <div id="root">
+        <header>
+            <div class="wrap header--flex">
+                <h1 class="header--logo"><a href="index.html">Courses</a></h1>
+                <nav>
+                    <ul class="header--signedin">
+                        <li>Welcome, Joe Smith!</li>
+                        <li><a href="sign-out.html">Sign Out</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <div class="wrap">
+                <h2>Create Course</h2>
+                <div class="validation--errors">
+                    <h3>Validation Errors</h3>
+                    <ul>
+                        <li>Please provide a value for "Title"</li>
+                        <li>Please provide a value for "Description"</li>
+                    </ul>
+                </div>
+                <form>
+                    <div class="main--flex">
+                        <div>
+                            <label for="courseTitle">Course Title</label>
+                            <input id="courseTitle" name="courseTitle" type="text" value="">
+
+                            <p>By Joe Smith</p>
+
+                            <label for="courseDescription">Course Description</label>
+                            <textarea id="courseDescription" name="courseDescription"></textarea>
+                        </div>
+                        <div>
+                            <label for="estimatedTime">Estimated Time</label>
+                            <input id="estimatedTime" name="estimatedTime" type="text" value="">
+
+                            <label for="materialsNeeded">Materials Needed</label>
+                            <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+                        </div>
+                    </div>
+                    <button class="button" type="submit">Create Course</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                </form>
+            </div>
+        </main>
+    </div>
+</body>
