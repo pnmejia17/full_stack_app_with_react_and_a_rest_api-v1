fac6046dfdeb5afdaecfea6bc4f6dc34fdb79df2
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-12-12 12:35:29 -0800

    fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  useEffect,
+  useState
+} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [courses, setCourses] = useState([])
 
-export default App;
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/courses`)
+      .then(res => {
+        setCourses(res)
+        console.log(res)
+        console.log(res.json())
+      })
+      .catch(error => console.log("Error fetching and parsing data", error))
+  }, [])
+
+
+  return ( 
+    <></>
+  // <>
+  //     <ul> {
+  //       courses.map(course => <li> {
+  //           course.title
+  //         } </li>)}</ul >
+  //         </>
+
+        );
+      }
+      export default App;
