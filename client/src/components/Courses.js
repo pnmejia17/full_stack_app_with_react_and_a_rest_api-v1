3ef08275b9d764065bffcee379f3fa22d629fcc8
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-12-18 09:32:36 -0800

    course detail bug

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -18,12 +18,13 @@ const Courses = () => {
             <div className="wrap main--grid">
                 {
                 courses.map((course) => (
-                <NavLink className="course--module course--link" to={`/api/courses/${course.id}`}>
+                <NavLink
+                key={course.id} className="course--module course--link" to={`/courses/${course.id}`}>
                     <h2 className="course--label">Course</h2>
                     <h3 className="course--title">{course.title}</h3>
                 </NavLink>))}
 
-                <NavLink className="course--module course--add--module" to={`api/courses/create`}>
+                <NavLink className="course--module course--add--module" to={`courses/create`}>
                     <span className="course--add--title">
                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                             viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
