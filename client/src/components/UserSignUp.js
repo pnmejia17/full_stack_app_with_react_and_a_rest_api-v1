b361aca97f9e01311d869d76c47da499a3b285e2
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-12-19 07:55:09 -0800

    Create User Sign Up

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,33 +1,72 @@
-<body>
-    <div id="root">
-        <header>
-            <div className="wrap header--flex">
-                <h1 className="header--logo"><a href="index.html">Courses</a></h1>
-                <nav>
-                    <ul className="header--signedout">
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="sign-in.html">Sign In</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>
-            <div className="form--centered">
-                <h2>Sign Up</h2>
-                
-                <form>
-                    <label for="firstName">First Name</label>
-                    <input id="firstName" name="firstName" type="text" value="">
-                    <label for="lastName">Last Name</label>
-                    <input id="lastName" name="lastName" type="text" value="">
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="">
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="">
-                    <button className="button" type="submit">Sign Up</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+import { useNavigate, useSearchParams } from "react-router-dom"
+import { api } from "../utils/apiHelper"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+
+function UserSignUp = () => {
+    const firstName = useRef(null)
+    const lastName = useRef(null)
+    const emailAddress = useRef(null)
+    const password = useRef(null)
+    const navigate = useNavigate()
+    const [errors, setErrors]= useState([])
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        navigate('/')
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const user = {
+            firstName: firstName.current.value,
+            lastName: lastName.current.value,
+            emailAddress: emailAddress.current.value,
+            password: password.current.value,
+        }
+
+    try {
+        const res = await api('/users', 'POST', user)
+        if (res.status === 201){
+            await actions.signIn(user)
+            navigate('/')
+        } else if (res.status === 400) {
+            const errorInfo = await res.json()
+            setErrors(errorInfo.errors)
+        } else {
+            throw new Error()
+        }
+        } catch (error) {
+            console.log(`Errors: ${error}`)
+        }
+     }
+
+    return(
+        <div className="form--centered">
+            <h2>Sign Up</h2>
+            { errors.length ?
+                <div className="validation--errors">
+                        <h3>Validation Errors</h3>
+                        <ul>
+                            {errors.map(error => <li>{error}</li>)}
+                        </ul>
+                </div> : null }    
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input id="firstName" name="firstName" type="text" value="" ref={firstName}/>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input id="lastName" name="lastName" type="text" value="" ref={lastName}/>
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" value="" ref={emailAddress}/>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" value="" ref={password}/>
+                    <button className="button" type="submit">Sign Up</button><button class="button button-secondary" onClick={handleCancel}>Cancel</button>
                 </form>
-                <p>Already have a user account? Click here to <a href="sign-in.html">sign in</a>!</p>
+                <p>Already have a user account? Click here to <Link to='/signin'>Sign In</Link>!</p>
             </div>
-        </main>
-    </div>
-</body>
+    )
+}
+
+
+
